bdfbe2f267e6da0b28f5c886b13ec9b55c7015a7
Rafael Nunes <angraralf@gmail.com>
2023-04-10 20:17:23 -0300

    second world first activities

---
+++ 02 - Floresta-Backwoods/1 - Defesa de Plainswood.js
@@ -0,0 +1,6 @@
+// Construa duas cercas nas marcas para manter os camponeses a salvo!
+// Passe o cursor do mouse sobre o terreno para obter as coordenadas X,Y.
+hero.moveUp();
+hero.buildXY("fence", 40, 52);
+hero.moveDown(2);
+hero.buildXY("fence", 40, 20);
---
+++ 02 - Floresta-Backwoods/2 - Trilha Sinuosa.js
@@ -0,0 +1,11 @@
+// Vá até o fim do caminho e construa uma cerca na marca X.
+// Use a função moveXY(x, y)
+
+// É o primeiro ponto do caminho.
+hero.moveXY(36, 59);
+// Mova-se até os próximos pontos do caminho.
+hero.moveXY(37, 13);
+// Ir até a 4 gemas 
+hero.moveXY(73, 61);
+// Construa uma cerca para parar o ogro.
+hero.buildXY("fence", 72, 25);
